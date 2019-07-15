85dc349cfb10e15c83a33ad3476f411ba077ade9
masonkarsevar <masonkarsevar@gmail.com>
2019-07-15 08:22:34 -0700

    placing click handlers on home team buttons

--- src/App.js
+++ src/App.js
@@ -31,8 +31,14 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button 
+          className="homeButtons__touchdown"
+          onClick={() => setAwayScore(homeScore + 1)}
+          >Home Touchdown</button>
+          <button 
+          className="homeButtons__fieldGoal"
+          onClick={() => setAwayScore(homeScore + 1)}
+          >Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown">Away Touchdown</button>
